6a93997a66fbf3f8c89cb2fe305ae4d8f3c43715
HARUYAMA Seigo <haruyama@unixuser.org>
2012-06-18 14:07:35 +0900

    update docstrings.

--- src/mixi/io.clj
+++ src/mixi/io.clj
@@ -1,11 +1,11 @@
-(ns'mixi.io)
+(ns 'mixi.io)
 
 (defn- ^String encoding [options]
   (or (:encoding options) "UTF-8"))
 
 (defn slurp
   "Reads the file named by f and returns it.
-  this manages resources better than core.slurp."
+  this manages resources better than clojure.core/slurp."
   [f & options]
   (let [sb (StringBuilder.)]
     (with-open [fis (java.io.FileInputStream. f)
@@ -20,7 +20,7 @@
 
 (defn spit
   "Opposite of slurp. Opens f with writer, writes content, then
-  closes f. this manages resources better than core.spit."
+  closes f. this manages resources better than clojure.core/spit."
   [f content & options]
   (with-open [fos (java.io.FileOutputStream. f)
               osw (apply java.io.OutputStreamWriter. fos (encoding options))
