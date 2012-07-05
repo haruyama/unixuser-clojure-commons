249ac42f6b48cd2f27bc36ad7cdb494461e7e636
HARUYAMA Seigo <haruyama@unixuser.org>
2012-07-05 15:13:26 +0900

    remove extract-field.

--- src/mixi/solr/solrj.clj
+++ src/mixi/solr/solrj.clj
@@ -4,10 +4,3 @@
 (defn get-results [server solr-query]
   (. (. server query solr-query) getResults))
 
-(defn extract-field [results field]
-  (map (fn [r]
-         (try
-           (. r getFieldValue field)
-           ; if field does not exist
-           (catch Exception e "")))
-       results))
