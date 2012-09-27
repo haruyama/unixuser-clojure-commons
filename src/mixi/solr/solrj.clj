a2a4a2be67a7c7d5519326bc639fc318c4f7c9c1
HARUYAMA Seigo <haruyama@unixuser.org>
2012-09-27 19:52:06 +0900

    (. hoge huga ...) -> (.huga hoge ...)

--- src/mixi/solr/solrj.clj
+++ src/mixi/solr/solrj.clj
@@ -5,24 +5,21 @@
 
 (defn ^SolrQuery make-query
   [^String q start rows sort-params & param-lists]
-  (let [
-        query (if q
+  (let [query (if q
                 (new SolrQuery q)
                 (new SolrQuery "*:*")
                 )
         ]
-    (. query setStart (Integer. start))
-    (. query setRows  (Integer. rows))
+    (.setStart query (Integer. start))
+    (.setRows query (Integer. rows))
     (doseq [sort-param sort-params]
       (if (= (second sort-param) "asc")
-        (. query addSortField (first sort-param) SolrQuery$ORDER/asc)
-        (. query addSortField (first sort-param) SolrQuery$ORDER/desc)))
+        (.addSortField query (first sort-param) SolrQuery$ORDER/asc)
+        (.addSortField query (first sort-param) SolrQuery$ORDER/desc)))
     (doseq [param-list param-lists]
       ( doseq [param param-list]
         (if (coll? (second param))
-          (. query setParam (first param) (into-array (second param)))
-          (. query setParam (first param) (into-array [(second param)]))))
-      )
+          (.setParam query (first param) (into-array (second param)))
+          (.setParam query (first param) (into-array [(second param)])))))
     query
-    )
-  )
+    ))
