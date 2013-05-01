ca10adb7c8acb653125ea83dd35b09b4d150ebfe
HARUYAMA Seigo <haruyama@unixuser.org>
2013-05-01 16:06:59 +0900

    update versions of libraries.

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.5.1"]
-                           [org.apache.solr/solr-core "4.2.0"]
-                           [org.apache.solr/solr-solrj "4.2.0"]
+                           [org.apache.solr/solr-core "4.2.1"]
+                           [org.apache.solr/solr-solrj "4.2.1"]
                            ]
             :profiles {:dev { :dependencies
-                             [[org.clojure/tools.namespace "0.2.2"]
+                             [[org.clojure/tools.namespace "0.2.3"]
                               [jonase/kibit "0.0.8"]]}}
             )
