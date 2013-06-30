1b380360e57e300525c87fc29a1ecbc265ab005a
HARUYAMA Seigo <haruyama@unixuser.org>
2013-06-30 17:51:33 +0900

    update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject haruyama/mixi-clojure-commons "0.1.0"
+(defproject haruyama/mixi-clojure-commons "0.2.0-SNAPSHOT"
             :description "a library for Clojure, used in mixi."
             :url "https://github.com/haruyama/mixi-clojure-commons"
             :license {:name "Eclipse Public License"
