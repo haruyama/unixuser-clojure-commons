fb2e0658dd7ea8fc26a3dc829a102a9c2d1bc4a5
HARUYAMA Seigo <haruyama@unixuser.org>
2013-06-30 16:15:17 +0900

    change name

--- project.clj
+++ project.clj
@@ -1,14 +1,12 @@
-(defproject mixi "0.1.0-SNAPSHOT"
+(defproject jp.co.mixi.rd/mixi-clojure-commons "0.1.0"
             :description "a library for Clojure, used in mixi."
-            :url "https://github.com/haruyama/mixi-cj-lib"
+            :url "https://github.com/haruyama/mixi-clojure-commons"
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.5.1"]
                            [org.apache.solr/solr-core "4.3.1"]
                            [org.apache.solr/solr-solrj "4.3.1"]
-                           [org.apache.lucene/lucene-analyzers-kuromoji "4.3.1"]
-                           ]
+                           [org.apache.lucene/lucene-analyzers-kuromoji "4.3.1"]]
             :profiles {:dev { :dependencies
                              [[org.clojure/tools.namespace "0.2.3"]
-                              [jonase/kibit "0.0.8"]]}}
-            )
+                              [jonase/kibit "0.0.8"]]}})
