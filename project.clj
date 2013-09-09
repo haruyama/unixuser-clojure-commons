df1376e84f8828b8870c4c69f67c6ec421a18633
HARUYAMA Seigo <haruyama@unixuser.org>
2013-09-10 01:08:36 +0900

    Lucene/Solr 4.4.0
    upgrade org.clojure/tools.namespace

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.5.1"]
-                           [org.apache.solr/solr-core "4.3.1"]
-                           [org.apache.solr/solr-solrj "4.3.1"]
-                           [org.apache.lucene/lucene-analyzers-kuromoji "4.3.1"]]
+                           [org.apache.solr/solr-core "4.4.0"]
+                           [org.apache.solr/solr-solrj "4.4.0"]
+                           [org.apache.lucene/lucene-analyzers-kuromoji "4.4.0"]]
             :profiles {:dev { :dependencies
-                             [[org.clojure/tools.namespace "0.2.3"]
+                             [[org.clojure/tools.namespace "0.2.4"]
                               [jonase/kibit "0.0.8"]]}})
--- test/mixi/lucene/analysis_test.clj
+++ test/mixi/lucene/analysis_test.clj
@@ -15,14 +15,14 @@
              (is (= ["私" "の" "名前" "は" "中野" "です"] (mixi.lucene.analysis/tokenize factory "私の名前は中野です")))
              (is (= ["関西国際空港"] (mixi.lucene.analysis/tokenize factory "関西国際空港")))))
 
-         (let [factory (org.apache.lucene.analysis.standard.StandardTokenizerFactory. (doto (java.util.HashMap.) (.put AbstractAnalysisFactory/LUCENE_MATCH_VERSION_PARAM "LUCENE_43")))]
+         (let [factory (org.apache.lucene.analysis.standard.StandardTokenizerFactory. (doto (java.util.HashMap.) (.put AbstractAnalysisFactory/LUCENE_MATCH_VERSION_PARAM "LUCENE_44")))]
            (is (= ["私" "の" "名" "前" "は" "中" "野" "で" "す"] (mixi.lucene.analysis/tokenize factory "私の名前は中野です")))
            (is (= ["関" "西" "国" "際" "空" "港"] (mixi.lucene.analysis/tokenize factory "関西国際空港")))))
 
 (deftest tokenizer-chain-test
-         (let [stf (org.apache.lucene.analysis.standard.StandardTokenizerFactory. (doto (java.util.HashMap.) (.put AbstractAnalysisFactory/LUCENE_MATCH_VERSION_PARAM "LUCENE_43")))
+         (let [stf (org.apache.lucene.analysis.standard.StandardTokenizerFactory. (doto (java.util.HashMap.) (.put AbstractAnalysisFactory/LUCENE_MATCH_VERSION_PARAM "LUCENE_44")))
                cwff (org.apache.lucene.analysis.cjk.CJKWidthFilterFactory. (java.util.HashMap.))
-               lcff (org.apache.lucene.analysis.core.LowerCaseFilterFactory. (doto (java.util.HashMap.) (.put AbstractAnalysisFactory/LUCENE_MATCH_VERSION_PARAM "LUCENE_43")))
+               lcff (org.apache.lucene.analysis.core.LowerCaseFilterFactory. (doto (java.util.HashMap.) (.put AbstractAnalysisFactory/LUCENE_MATCH_VERSION_PARAM "LUCENE_44")))
                cbff (org.apache.lucene.analysis.cjk.CJKBigramFilterFactory. (java.util.HashMap.))]
            (with-open [tokenizer-chain (org.apache.solr.analysis.TokenizerChain. stf (into-array org.apache.lucene.analysis.util.TokenFilterFactory [cwff lcff cbff]))]
              (is (= ["lupin" "the" "third"] (mixi.lucene.analysis/tokenize tokenizer-chain "Lupin The Third")))
